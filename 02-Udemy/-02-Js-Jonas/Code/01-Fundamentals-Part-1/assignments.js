a65c59d44c86d2255d264e9af9accaaa5d1d87de
Youssef_Mahmoud <100860879+Youssef-Mhmoud@users.noreply.github.com>
2023-01-09 00:58:05 +0200

    finish section javascript fundamentals part 1

--- 02-Udemy/-02-Js-Jonas/Code/01-Fundamentals-Part-1/assignments.js
+++ 02-Udemy/-02-Js-Jonas/Code/01-Fundamentals-Part-1/assignments.js
@@ -1,4 +1,4 @@
-// LECTURE: VARIABLES AND VALUES
+// LECTURE: Values and Variables
 
 const country = "Egypt";
 const continent = "Africa";
@@ -8,9 +8,9 @@ let population = 170;
 // console.log(continent)
 // console.log(population)
 //------------------------------------------
-// LECTURE: DATA TYPES
+// LECTURE: Data Types
 
-const isIsland = country === "Egypt";
+let isIsland = false;
 let language;
 
 // console.log(typeof isIsland)
@@ -18,14 +18,17 @@ let language;
 // console.log(typeof country)
 // console.log(typeof language)
 //------------------------------------------
-// LECTURE: LET, CONST, VAR
+// LECTURE: let, const and var
 
-language = "Arabic";
+language = "arabic";
+
+// const isIsland = false
+isIsland = true;
 
 // ERROR:
 // country = 'another'
 //------------------------------------------
-// LECTURE: BASIC OPERATOR
+// LECTURE: Basic Operators
 
 let countryHalf = population / 2;
 
@@ -50,11 +53,11 @@ let description =
 
 // console.log(description)
 //------------------------------------------
-// LECTURE: STRINGS AND TEMPLATE LITERALS
+// LECTURE: Strings and Template Literals
 description = `${country} is in ${continent}, and its ${population} million people speak ${language}`;
 
 //------------------------------------------
-// LECTURE: TAKING DECISIONS: IF/ELSE
+// LECTURE: Taking Decisions: if / else Statements
 
 // population = 13
 // population = 130
@@ -66,4 +69,66 @@ description = `${country} is in ${continent}, and its ${population} million peop
 // }
 
 //------------------------------------------
-// LECTURE: TAKING DECISIONS: IF/ELSE
+// LECTURE: Type Conversion and Coercion
+
+// console.log('9' - '5') // 4
+// console.log('19' - '13' + '17'); // -> '617'
+// console.log('19' - '13' + 17); // -> 23
+// console.log('123' < 57) // false
+// console.log(5 + 6 + '4' + 9 - 4 - 2) // 1143
+
+//------------------------------------------
+// LECTURE: Equality Operators: == vs. ===
+
+// const numNeighbours = Number(
+//   prompt("How many neighbour countries does your country have?")
+// );
+
+// if (numNeighbours === 1) {
+//   console.log("Only 1 border");
+// } else if (numNeighbours > 1) {
+//   console.log("More than 1 border");
+// } else {
+//   console.log("No borders");
+// }
+
+//------------------------------------------
+// LECTURE: Logical Operators
+
+// if (language === 'English' && population < 55 && !isIsland) {
+//   console.log(`You should live in ${country} :)`)
+// } else {
+//   console.log(`${country} does not meet your criteria :(`)
+// }
+
+//------------------------------------------
+// LECTURE: The switch Statement
+/*
+switch (language) {
+  case "chinese":
+  case "mandarin":
+    console.log("MOST number of native speakers!");
+    break;
+  case "spanish":
+    console.log("2nd place in number of native speakers");
+    break;
+  case "english":
+    console.log("3rd place");
+    break;
+  case "hindi":
+    console.log("Number 4");
+    break;
+  case "arabic":
+    console.log("5th most spoken language");
+    break;
+  default:
+    console.log("Great language too :D");
+}
+*/
+//------------------------------------------
+// LECTURE: The Conditional (Ternary) Operator
+// population = 13
+
+// console.log(
+//   `${country}'s population is ${population > 33 ? "above" : "below"} average`
+// );
--- 02-Udemy/-02-Js-Jonas/Code/01-Fundamentals-Part-1/script.js
+++ 02-Udemy/-02-Js-Jonas/Code/01-Fundamentals-Part-1/script.js
@@ -2,7 +2,7 @@
 //   alert("JavaScript is FUN!");
 // }
 // 40 + 8 + 23 - 10;
-//----------------------- VARIABLES AND VALUES -----------------------
+//----------------------- Values and Variables -----------------------
 /*
 let js = "amazing";
 // console.log(40 + 8 + 23 - 10);
@@ -49,7 +49,7 @@ let job2 = "Teacher";
 console.log(myFirstJob)
 */
 /////////////////////////////////////////////////////////////////////
-//----------------------- DATA TYPES -----------------------
+//----------------------- Data Types -----------------------
 /*
 let javaScriptIsFun = true;
 console.log(javaScriptIsFun);
@@ -72,7 +72,7 @@ console.log(typeof year);
 console.log(typeof null)
 */
 /////////////////////////////////////////////////////////////////////
-//----------------------- LET, CONST, VAR -----------------------
+//----------------------- let, const and var -----------------------
 /*
 let age = 30;
 age = 31;
@@ -88,7 +88,7 @@ lastName = 'Mahmoud'
 console.log(lastName)
 */
 /////////////////////////////////////////////////////////////////////
-//----------------------- BASIC OPERATORS -----------------------
+//----------------------- Basic Operators -----------------------
 /*
 // Math operators
 const now = 2037;
@@ -137,7 +137,7 @@ const averageAge = (ageYoussef + ageAhmed) / 2;
 console.log(ageYoussef, ageAhmed);
 */
 /////////////////////////////////////////////////////////////////////
-//----------------------- STRINGS AND TEMPLATE LITERALS -----------------------
+//----------------------- Strings and Template Literals -----------------------
 /*
 const firstName = "Youssef";
 const job = "teacher";
@@ -162,7 +162,8 @@ multiple
 lines`)
 */
 /////////////////////////////////////////////////////////////////////
-//----------------------- TAKING DECISIONS: IF/ELSE -----------------------
+//----------------------- Taking Decisions: if / else Statements -----------------------
+/*
 const age = 15;
 // const isOldEnough = age >= 18;
 
@@ -183,3 +184,174 @@ if (birthYear <= 2000) {
 }
 
 console.log(century);
+*/
+/////////////////////////////////////////////////////////////////////
+//----------------------- Type Conversion and Coercion-----------------------
+/*
+// type conversion
+const inputYear = "1991";
+console.log(Number(inputYear), inputYear);
+console.log(Number(inputYear) + 18);
+
+console.log(Number("Youssef"));
+console.log(typeof NaN);
+
+console.log(String(23), 23);
+
+// type coercion
+console.log("I am " + 23 + " years old");
+console.log("23" - "10" - 3);
+console.log("23" / "2");
+
+let n = "1" + 1; // '11'
+n = n - 1;
+console.log(n)
+
+// 5 falsy values: 0, '', undefined, null, NaN
+
+console.log(Boolean(0));
+console.log(Boolean(undefined));
+console.log(Boolean("Youssef"));
+console.log(Boolean({}));
+console.log(Boolean(""));
+
+const money = 100;
+if (money) {
+  console.log("Don't spend it all ;)");
+} else {
+  console.log("You should get a job!");
+}
+
+let height = 0;
+if (height) {
+  console.log("YAY! Height is defined");
+} else {
+  console.log("Height is UNDEFINED");
+}
+*/
+/////////////////////////////////////////////////////////////////////
+//----------------------- Equality Operators: == vs. === -----------------------
+/*
+const age = "18";
+if (age === 18) console.log("You just became an adult :D (strict)");
+
+if (age == 18) console.log("You just became an adult :D (loose)");
+
+const favourite = Number(prompt("What's your favourite number?"));
+console.log(favourite);
+console.log(typeof favourite);
+
+if (favourite === 23) {
+  console.log("Cool! 23 is an amazing number!");
+} else if (favourite === 7) {
+  console.log("7 is also a cool number");
+}  else if (favourite === 9) {
+  console.log("9 is also a cool number");
+} else {
+  console.log("Number is not 23 or 7 or 9");
+}
+
+if(favourite !== 23) console.log('Why not 23?')
+*/
+/////////////////////////////////////////////////////////////////////
+//----------------------- Logical Operators -----------------------
+/*
+const hasDriversLicense = true; // A
+const hasGoodVision = true; // B
+
+console.log(hasDriversLicense && hasGoodVision);
+console.log(hasDriversLicense || hasGoodVision);
+console.log(!hasDriversLicense);
+
+// if (hasDriversLicense && hasGoodVision) {
+//   console.log("Ahmed is able to drive!");
+// } else {
+//   console.log("Someone else should drive...");
+// }
+
+const isTired = false; // C
+console.log(hasDriversLicense && hasGoodVision && isTired);
+
+if (hasDriversLicense && hasGoodVision && !isTired) {
+  console.log("Ahmed is able to drive!");
+} else {
+  console.log("Someone else should drive...");
+}
+*/
+/////////////////////////////////////////////////////////////////////
+//----------------------- The switch Statement -----------------------
+/*
+const day = "friday";
+
+switch (day) {
+  case "monday": // day === 'monday'
+    console.log("Plan course structure");
+    console.log("Go to coding meetup");
+    break;
+  case "tuesday":
+    console.log("Prepare theory videos");
+    break;
+  case "wednesday":
+  case "thursday":
+    console.log("Write code examples");
+    break;
+  case "friday":
+    console.log("Record videos");
+    break;
+  case "saturday":
+  case "sunday":
+    console.log("Enjoy the weekend :D");
+    break;
+  default:
+    console.log("Not a valid day!");
+}
+
+if (day === "monday") {
+  console.log("Plan course structure");
+  console.log("Go to coding meetup");
+} else if (day === "tuesday") {
+  console.log("Prepare theory videos");
+} else if (day === "wednesday" || day === "thursday") {
+  console.log("Write code examples");
+} else if (day === "friday") {
+  console.log("Record videos");
+} else if (day === "saturday" || day === "sunday") {
+  console.log("Enjoy the weekend :D");
+} else {
+  console.log("Not a valid day!");
+}
+*/
+/////////////////////////////////////////////////////////////////////
+//----------------------- Statements and Expressions -----------------------
+/*
+// Expressions
+3 + 4;
+1991;
+true && false && !false;
+
+// Statements
+if (23 > 18) {
+  const str = "23 is bigger";
+}
+const me = "Youssef";
+console.log(`i'm ${2037 - 1991} years old ${me}`);
+*/
+/////////////////////////////////////////////////////////////////////
+//----------------------- The Conditional (Ternary) Operator -----------------------
+// const age = 23;
+//  age >= 18
+//    ? console.log("I like do drink water 💧")
+//    : console.log("I like to drink juice 🧃");
+
+// const drink = age >= 18 ? "water 💧" : "juice 🧃";
+// console.log(drink);
+
+// let drink2;
+// if (age >= 18) {
+//   drink2 = "water 💧";
+// } else {
+//   drink2 = "juice 🧃";
+// }
+// console.log(drink2);
+
+// console.log(`I like to drink ${age >= 18 ? "water 💧" : "juice 🧃"}`);
